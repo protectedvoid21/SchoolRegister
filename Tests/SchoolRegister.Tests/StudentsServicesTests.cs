112cf0abc981977bfa956e6a43232699132e3fbe
protectedvoid21 <nieznanyzero@interia.pl>
2022-07-07 14:14:13 +0200

    Project restructurization

---
+++ Tests/SchoolRegister.Tests/StudentsServicesTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using SchoolRegister.Models;
+using SchoolRegister.Services.Students;
+using SchoolRegister.Services.Subjects;
+using SchoolRegister.Tests.Utils;
+using Xunit;
+
+namespace SchoolRegister.Tests;
+
+public class StudentsServiceTests {
+    [Theory]
+    [InlineData("Joe", "Doe")]
+    [InlineData("Adam", "Żółć")]
+    [InlineData("Michael", "Gray")]
+    public async Task Add_Students_ExistsInDb(string name, string surname) {
+        var options = new DbContextOptionsBuilder<SchoolRegisterContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+        var context = new SchoolRegisterContext(options);
+
+        var userManagerMock = new Mock<FakeUserManager>();
+        var subjectsServiceMock = new Mock<ISubjectsService>();
+        var studentsService = new StudentsService(context, userManagerMock.Object, subjectsServiceMock.Object);
+
+        await studentsService.AddAsync(name, surname, 0);
+        
+        var students = await studentsService.GetAllAsync();
+
+        Assert.Single(students);
+    }
+}
---
+++ Tests/SchoolRegister.Tests/Utils/FakeUserManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using SchoolRegister.Models;
+
+namespace SchoolRegister.Tests.Utils; 
+
+public class FakeUserManager : UserManager<AppUser> {
+    public FakeUserManager()
+        : base(new Mock<IUserStore<AppUser>>().Object,
+            new Mock<IOptions<IdentityOptions>>().Object,
+            new Mock<IPasswordHasher<AppUser>>().Object,
+            Array.Empty<IUserValidator<AppUser>>(),
+            Array.Empty<IPasswordValidator<AppUser>>(),
+            new Mock<ILookupNormalizer>().Object,
+            new Mock<IdentityErrorDescriber>().Object,
+            new Mock<IServiceProvider>().Object,
+            new Mock<ILogger<UserManager<AppUser>>>().Object) {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+    }
+}
